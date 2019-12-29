1a03c621b487ea8637ce05c8feabeb5110353382
Roger Cheng <roger.random@outlook.com>
2019-12-29 02:43:52 -0800

    Verify well-formedness of calculated results

--- scripts/unittests.py
+++ scripts/unittests.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import chassis_wheel_calculator as cwc
 import unittest
+import math
+
+max_angle = 2 * math.pi
 
 class TestWheelCalculator(unittest.TestCase):
     def setUp(self):
+        """Configure test chassis with default Sawppy dimensions"""
         self.test_chassis = list()
 
         self.test_chassis.append(cwc.chassis_wheel('front_left',  11.375,   9.125))
@@ -15,10 +19,28 @@ class TestWheelCalculator(unittest.TestCase):
 
         self.calculator = cwc.chassis_wheel_calculator(self.test_chassis)
 
+    def well_formed_results(self, results):
+        """Verify calculated results are in expected form"""
+        self.assertEqual(len(results), 6, "Six wheels in should have resulted in six wheels out")
+
+        test_names_checklist=list(['front_left', 'front_right', 'mid_left', 'mid_right', 'rear_left', 'rear_right'])
+        test_names_encountered=list()
+
+        for wheel in results:
+            self.assertIn(wheel.name, test_names_checklist, "Encountered name not on the checklist")
+            test_names_encountered.append(wheel.name)
+            test_names_checklist.remove(wheel.name)
+            self.assertGreaterEqual(wheel.angle, -max_angle)
+            self.assertLessEqual(wheel.angle, max_angle)
+
+        self.assertEqual(len(test_names_checklist), 0, "Wheel names were expected but never encountered: {0}".format(test_names_checklist))
+        self.assertEqual(len(test_names_encountered), 6, "Less than six names on the checklist was encountered")
+
     def test_straight_forward(self):
         results = self.calculator.calculate(0, 1.0)
 
-        self.assertEqual(len(results), 6, "Six wheels in should have resulted in six wheels out")
+        self.well_formed_results(results)
+
         for wheel in results:
             print(wheel.name, wheel.angle, wheel.velocity)
 
